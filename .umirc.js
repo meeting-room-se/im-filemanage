d5b2d29a3c682e8669a391de93176e45f82c46ac
zhaomanzhou <zmzsstreet@163.com>
2020-08-11 15:50:55 +0800

    initial commit

---
+++ .umirc.js
@@ -0,0 +1,14 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ]
+}
---
+++ src/pages/room/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class Room extends React.Component{
+
+
+  render() {
+    return(
+      <button/>
+    )
+  }
+
+}
