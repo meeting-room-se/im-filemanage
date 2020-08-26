d9775f4592a0d6cb0793c5d7a979875b49c4491a
bxnldxnfw <1094307787@qq.com>
2020-08-26 11:40:24 +0800

    8.26 11:40

--- src/pages/filemanage/components/fileContent/index.js
+++ src/pages/filemanage/components/fileContent/index.js
@@ -7,6 +7,32 @@ import icon from '../../statics/iconfont/iconfont.css'
 function FileContent(props){
   const columns = props.columns;
   const data = props.data;
+  // 是否显示上传路径函数
+  function UploadPath(){
+    if(props.havepath){
+      return <div className={styles.UploadPath}>
+        <Form>
+          <Form.Item
+            label="Path"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+            key={props.path}
+            initialValue={props.path}
+            style={{ marginBottom: "0" }}
+          >
+            <Input style={{width: "350px"}} value={props.path}/>
+          </Form.Item>
+        </Form>
+      </div>
+    }
+  }
+
+  function getBase64(img, callback){
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    // 读取文件
+    reader.readAsDataURL(img);
+  }
 
   return (
     <div className={styles.Wrapper}>
@@ -42,17 +68,25 @@ function FileContent(props){
             <Upload
               name="file"
               multiple={true}
-              action={props.uploadurl}
+
               onChange={
                 (info) => {
                   const { status } = info.file;
                   if (status !== 'uploading') {
-                    console.log(info.file, info.fileList);
                   }
                   if (status === 'done') {
+                    props.dispatch({
+                      type: 'uploadModal/addFile',
+                      payload: info.file
+                    })
                     message.success(`${info.file.name} file uploaded successfully.`);
                   } else if (status === 'error') {
                     message.error(`${info.file.name} file upload failed.`);
+                  } else if (status === 'remove'){
+                    props.dispatch({
+                      type: 'uploadModal/removeFile',
+                      payload: info.file
+                    })
                   }
                 }
               }
@@ -61,37 +95,24 @@ function FileContent(props){
                 click here or drop file here
               </div>
             </Upload>
-            <Radio.Group value={props.radiovalue} style={{ marginTop: "20px",marginLeft: "120px" }} onChange={
-              (e) => {
-                props.dispatch({
-                  type: 'uploadModal/changeRadio',
-                  payload: {
-                    radiovalue: e.target.value
-                  }
-                })
-              }
-            }>
+            {/*上传类型*/}
+            <Radio.Group value={props.radiovalue} hidden style={{ marginTop: "20px",marginLeft: "120px"  }} onChange={
+            (e) => {
+              props.dispatch({
+                type: 'uploadModal/changeRadio',
+                payload: {
+                  radiovalue: e.target.value
+                }
+              })
+            }
+          }>
               <Radio value={1}>UnKeepName</Radio>
               <Radio value={2}>KeepName</Radio>
             </Radio.Group>
-            <br/>
-            <div className={styles.UploadPath}>
-              <Form>
-                <Form.Item
-                  label="Path"
-                  name="username"
-                  rules={[{ required: true, message: 'Please input your username!' }]}
-                  key={props.path}
-                  initialValue={props.path}
-                >
-                  <Input style={{width: "350px"}} value={props.path}/>
-                </Form.Item>
-              </Form>
-
-            </div>
-
-
-
+            {/*判断是否显示上传路径*/}
+            {
+              UploadPath()
+            }
 
           </Modal>
         </div>
@@ -108,7 +129,8 @@ function mapStateToProps({ tableContent,uploadModal  }){
     path: tableContent.path,
     uploadurl: tableContent.uploadurl,
     radiovalue: uploadModal.radiovalue,
-    visible: uploadModal.visible
+    visible: uploadModal.visible,
+    havepath: uploadModal.havepath
   }
 }
 
--- src/pages/filemanage/models/uploadModal.js
+++ src/pages/filemanage/models/uploadModal.js
@@ -6,16 +6,35 @@ export default {
   state: {
     visible: false,
     radiovalue: 1,
-    uploadurl: remoteurl+"/file/upload/headpic"
+    uploadurl: remoteurl+"/file/upload/headpic",
+    havepath: false,
+    filelist: []
   },
   reducers: {
     changeRadio(state, { payload }){
       if(payload["radiovalue"] === 1){
         payload["uploadurl"] = remoteurl+"/file/upload/headpic";
+        payload["havepath"] = false;
+
       }else{
-        payload["uploadurl"] = remoteurl+"/file/upload/small"
+        payload["uploadurl"] = remoteurl+"/file/upload/small";
+        payload["havepath"] = true;
       }
       return setState(state,payload);
+    },
+
+    changeFileList(state, { payload }){
+      return setState(state,payload);
+    },
+
+    addFile(state, { payload }){
+      const payload_ = {
+        "filelist": [...state.filelist,payload]
+      }
+      return setState(state,payload_);
+    },
+
+    removeFile(state, { payload }){
     },
 
     changeVisible(state,{ payload }){
